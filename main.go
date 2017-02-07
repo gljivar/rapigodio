fb1c1145bd860c8cc9e2e00500b749ceeb8e3470
gljivar <gljivar@gmail.com>
2017-02-07 00:00:43 +0000

    Add radio start method

--- main.go
+++ main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+  "syscall"
+  "os"
+  "os/exec"
+)
+
+type stationInfo struct {
+  name string
+  streamIpAddress string
+}
+
+func startRadio(name string, streamIpAddress string) {
+  binary, _ := exec.LookPath("mplayer")
+  args := []string{"mplayer", streamIpAddress}
+
+  env := os.Environ()
+
+  syscall.Exec(binary, args, env)
+}
+
+func main() {
+  station := stationInfo{name: "otvoreni", streamIpAddress: "http://50.7.129.122:8249/;"} 
+
+  startRadio(station.name, station.streamIpAddress)
+
+}
