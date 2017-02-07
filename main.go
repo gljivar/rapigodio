60c80cd29851d1486412ea38b2a223c164b9374a
gljivar <gljivar@gmail.com>
2017-02-07 01:30:25 +0000

    Experiments for calling mplayer

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
   "syscall"
   "os"
   "os/exec"
+  "fmt"
+  "bufio"
 )
 
 type stationInfo struct {
@@ -12,9 +14,11 @@ type stationInfo struct {
 }
 
 func startRadio(name string, streamIpAddress string) {
-  binary, _ := exec.LookPath("mplayer")
-  args := []string{"mplayer", streamIpAddress}
-
+  //binary, _ := exec.LookPath("mplayer")
+  //args := []string{"mplayer", streamIpAddress, "-really-quiet"}
+ 
+  binary, _ := exec.LookPath("ls") 
+  args := []string{"ls"}
   env := os.Environ()
 
   syscall.Exec(binary, args, env)
@@ -23,6 +27,35 @@ func startRadio(name string, streamIpAddress string) {
 func main() {
   station := stationInfo{name: "otvoreni", streamIpAddress: "http://50.7.129.122:8249/;"} 
 
-  startRadio(station.name, station.streamIpAddress)
+  quit := make(chan bool)
+  
+  go func(name string, streamIpAddress string) {
+    started := false
+    for {
+      select {
+      case <- quit:
+        return
+      default:
+        if !started {
+          startRadio(station.name, station.streamIpAddress) 
+        } 
+        started = true
+      }
+    }
+  }("t", "t")
+  //go startRadio(station.name, station.streamIpAddress)
+ 
+  scanner := bufio.NewScanner(os.Stdin)
+    var text string
+    for text != "q" {  // break the loop if text == "q"
+        fmt.Print("Enter your text: ")
+        scanner.Scan()
+        text = scanner.Text()
 
+            fmt.Println("Your text was: ", text)
+        if text == "s" {
+            quit <- true
+            fmt.Println("Your text was: ", text)
+        }
+    }
 }
