b12a6e799cea47155fbf11727fb26f9cec3b9f0f
gljivar <gljivar@gmail.com>
2017-02-09 23:53:55 +0000

    Move stations to file

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
   "strconv"
   "radio"
   "flag"
+  "encoding/json"
+  "io/ioutil"
 )
 
 type StationInfo struct {
@@ -28,37 +30,21 @@ var templates = template.Must(template.ParseFiles("index.html"))
 var playValidPath = regexp.MustCompile("^/(play)/(\\d)/*(.*)$")
 var radioStatus RadioStatus = RadioStatus{}
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-  var stations []StationInfo
-  stations = append(stations, StationInfo{
-    Id: 1, 
-    Name: "Yammat", 
-    StreamIpAddress: "http://192.240.102.133:12430/stream;", 
-    IconAddress: "https://thumbnailer.mixcloud.com/unsafe/128x128/profile/3/f/6/0/211e-ddbd-422b-9f89-d19ef718bb63.jpg",
-    ImageAddress: "http://elelur.com/data_images/articles/happy-dogs-do-you-know-what-makes-them-really-so.jpg"})
-  stations = append(stations, StationInfo{
-    Id: 2, 
-    Name: "Otvoreni", 
-    StreamIpAddress: "http://50.7.129.122:8249/;",
-    IconAddress: "https://pbs.twimg.com/profile_images/821661264954986496/Uw-QUr9u_reasonably_small.jpg",
-    ImageAddress: "http://i.imgur.com/FPiTgfC.jpg"})  
-  stations = append(stations, StationInfo{
-    Id: 3, 
-    Name: "Czworka", 
-    StreamIpAddress: "http://stream3.polskieradio.pl:8956/;.mp3",
-    IconAddress: "http://player.polskieradio.pl/Content/_img/pr4-logo.png",
-    ImageAddress: "https://larrycraven.files.wordpress.com/2011/02/happy-dog1.jpg"})  
-  stations = append(stations, StationInfo{
-    Id: 4,
-    Name: "BBC 6",
-    StreamIpAddress: "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_6music_mf_p",
-    IconAddress: "http://1.bp.blogspot.com/-nxf-VYZ5JTw/TzZWg4psx8I/AAAAAAAAAqM/0av78qhJ_WQ/s1600/bbcradio6.jpg",
-    ImageAddress: "http://www.madewithhappy.com/wp-content/uploads/happy-dog2.jpg"})
-
-  if radioStatus.NowPlaying == (StationInfo{}) {
-    radioStatus = RadioStatus{Stations: stations}
+func loadStations(filename string) ([]StationInfo) {
+  fileContent, err := ioutil.ReadFile(filename)
+  if err != nil {
+    panic(err) 
   }
+  var stations []StationInfo 
+  if err := json.Unmarshal(fileContent, &stations); err != nil {
+    panic(err)
+  }
+  fmt.Println(stations)
 
+  return stations 
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
   err := templates.ExecuteTemplate(w, "index.html", radioStatus)
 
   if err != nil {
@@ -109,6 +95,9 @@ func choose(ss []StationInfo, test func(StationInfo) bool) (ret []StationInfo) {
 func main() {
   port := flag.Int("p", 8080, "Port") 
   flag.Parse()
+
+  filename := "stations.json"
+  radioStatus.Stations = loadStations(filename)
 
   runtime.GOMAXPROCS(2)
   radio.Initialize()
